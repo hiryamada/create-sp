9fd1bf99cc4ed1e40b17fa956153e67a2e4a451e
hiryamada <71864722+hiryamada@users.noreply.github.com>
2023-03-11 23:18:07 +0900

    macOS

--- setup-macos.sh
+++ setup-macos.sh
@@ -1,3 +1,9 @@
+# install required software
+brew install azure-cli jq
+
+# log in to Azure
+az login
+
 # create new sp
 curl https://raw.githubusercontent.com/hiryamada/create-sp/main/create-sp.sh |bash
 
